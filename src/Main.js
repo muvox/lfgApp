c7aac204cd6470676195a392a6d624dd3f97ba4f
Joni Niemi <muvoksi@gmail.com>
2018-11-26 08:59:42 +0200

    retry

---
+++ src/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Loading } from './components/common/';
+import Auth from './screens/Auth';
+import LoggedIn from './screens/LoggedIn';
+
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jwt: '',
+    }
+  }
+
+  render() {
+    if(!this.state.jwt) {
+      return (
+        <Auth />
+      );
+    } else if(this.state.jwt) {
+      return (
+        <LoggedIn />
+      );
+    }
+  }
+}
