0da9ccb91f23e0e6028d63265c7b0144279807b1
muvox <muvoksi@gmail.com>
2018-11-30 19:20:46 +0200

    Tuning toasts at register/login screen. Handling errors from server. WIP

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Keyboard } from "react-native";
 import { Input, TextLink, Button, Loading } from './common';
 import axios from 'axios';
 import deviceStorage from '../services/deviceStorage';
+import Toast, {DURATION} from 'react-native-easy-toast';
 
 class Registration extends React.Component {
   constructor(props) {
@@ -15,7 +16,7 @@ class Registration extends React.Component {
     console.log('starting the register method');
     const { username, password, error, loading } = this.state;
     this.setState({ error: '', loading: true});
-
+    Keyboard.dismiss();
     console.log('const and state set');
 
     const user = {
@@ -26,23 +27,30 @@ class Registration extends React.Component {
     console.log(user);
 
     //localhost address: http://192.168.1.106:8080/users/sign-up
-
-    axios.post(`http://lookingforgamer.herokuapp.com/users/sign-up`,
+    //deployment address: http://lookingforgamer.herokuapp.com/users/sign-up
+    axios.post(`http://192.168.1.106:8080/users/sign-up`,
       user,
       {headers: {
           'Content-Type': 'application/json'
       }})
     .then((response) => {
       console.log(response);
+      console.log(response.data);
+      //TODO: käy läpi taken username tässä, laastarifiksillä
+      this.refs.successToast.show('Registration succesful, please log in.');
+      this.setState({loading: false});
+
     })
     .catch((error) => {
       console.log(error);
+      this.refs.errorToast.show('Something went wrong!');
+
     });
   }
 
   render() {
     const { username, password, error, loading } = this.state;
-    const { form, section, errorTextStyle } = styles;
+    const { toastSuccess, toastError, form, section, errorTextStyle } = styles;
 
     return (
       <Fragment>
@@ -85,6 +93,8 @@ class Registration extends React.Component {
 
         </View>
 
+        <Toast ref="successToast" style={toastSuccess}/>
+        <Toast ref="errorToast" style={toastError}/>
 
       </Fragment>
     );
@@ -92,6 +102,14 @@ class Registration extends React.Component {
 }
 
 const styles = {
+  toastSuccess: {
+  backgroundColor: '#ccf2e8',
+  borderColor: '#b8ecdf'
+  },
+  toastError:{
+  backgroundColor: '#fadbd8',
+  borderColor: '#f8cdc8'
+ },
   form: {
     width: "100%",
     borderTopWidth: 1,
